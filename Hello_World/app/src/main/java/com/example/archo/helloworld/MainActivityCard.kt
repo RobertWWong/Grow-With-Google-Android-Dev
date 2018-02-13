5f074b4c2067692d8c1fc4e1b2ba5c793ffc04f4
Robertwwong <robertww@uci.edu>
2018-02-12 18:11:12 -0800

    Finished with the card, low-Q copy and pasta but it's something. I want to take a class on visual and app design. Seems very useful. PC LilyPichu (I ain't no thief)

---
+++ Hello_World/app/src/main/java/com/example/archo/helloworld/MainActivityCard.kt
@@ -0,0 +1,15 @@
+package com.example.archo.helloworld
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Rob Wong on 2/12/2018.
+ */
+
+class MainActivityCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    
+}
