9dfbc6591f1803440e6af21a3a68e5a7eb880712
Robert Wong <robertww@uci.edu>
2018-03-09 11:55:23 -0800

    Initial kotlin state

--- PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.kt
+++ PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.android.practiceset2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
+    @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    //Display methods to screen
+
+    public fun display(text: String){
+        val t = findViewById<TextView>(R.id.display_text_view)
+        t.text = text
+    }
+
+    public fun display(num: Int) {
+        val t = findViewById<TextView>(R.id.display_text_view)
+        t.text = num + ""
+    }
+
+    public fun display1(text: String) {
+        display(text)
+    }
+
+    public fun display2(text: String) {
+        val t = findViewById<TextView>(R.id.display_text_view_2)
+        t.text = text
+    }
+
+    public fun display3(text: String) {
+        val t = findViewById<TextView>(R.id.display_text_view_3)
+        t.text = text
+    }
+
+
 }
