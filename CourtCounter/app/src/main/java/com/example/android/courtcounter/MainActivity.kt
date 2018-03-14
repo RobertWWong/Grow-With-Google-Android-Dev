814f856f5eee90cfc2c822736e5a851d6785232e
Robert Wong <robertww@uci.edu>
2018-03-14 15:14:10 -0700

    Create a universal score tracker function
    pointMade function listens and updates team scores appropriately
    The View object contains all IDs created in the XML
    This allows for modifications of values without excessive rewriting.
    
    Fix #2

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.kt
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.kt
@@ -4,12 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.view.*
-
 
 class MainActivity : AppCompatActivity() {
-//    https://stackoverflow.com/questions/25905086/multiple-buttons-onclicklistener-android
-    private var score = 0
+    private var scoreA = 0
+    private var scoreB = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +15,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Displays the given score for Team A.
-     * After a given int argument
+     * Displays the given score for Team A and B
      */
-    fun displayForTeamA() {
-        val scoreView = findViewById(R.id.team_a_score) as TextView
-        scoreView.text = score.toString()
+    fun displayForTeams() {
+        val teamAview = findViewById(R.id.team_a_score) as TextView
+        teamAview .text = scoreA.toString()
+
+        val teamBview = findViewById(R.id.team_b_score) as TextView
+        teamBview .text = scoreB.toString()
     }
 
+    /**
+    *Reset scores for both team, then displays it on the screen
+    */
+    fun resetScores (view: View){
+        scoreA = 0
+        scoreB = 0
+        displayForTeams()
+    }
+
+    /**
+    * Determine which team gets a point via the view id property
+    */
     fun pointMade(view: View) {
-//        var point_view = findViewById<View>(R.id.team_a_score) as TextView
-        when(view.id){      //This is the switch function
-            R.id.three_p -> score += 3
-            R.id.two_p -> score += 2
-            R.id.free_p -> score += 1
+        when(view.id){      //This is the switch function in Kotlin
+            //Team A decisions
+            R.id.three_pa -> scoreA += 3
+            R.id.two_pa -> scoreA += 2
+            R.id.free_pa -> scoreA += 1
+            //Team B decisions
+            R.id.three_pb -> scoreB += 3
+            R.id.two_pb -> scoreB += 2
+            R.id.free_pb -> scoreB += 1
         }
-        displayForTeamA()
+        displayForTeams()
     }
 
 }
