0363d91b24b097d1aac11a75bb6efecae3a0fe7b
Robert Wong <robertww@uci.edu>
2018-03-21 16:51:08 -0700

    Be aware of logging states:
    e(String, String) (error)
    w(String, String) (warning)
    i(String, String) (information)
    d(String, String) (debug)
    v(String, String) (verbose)

--- Menu/app/src/main/java/com/example/android/menu/MainActivity.kt
+++ Menu/app/src/main/java/com/example/android/menu/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.android.menu
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +16,17 @@ class MainActivity : AppCompatActivity() {
 
     fun printToLogs(view: View) {
         // Find first menu item TextView and print the text to the logs
-
+        val first = findViewById<TextView>(R.id.menu_item_1)
+        Log.v("Within printToLogs", first.text as String?)
         // Find second menu item TextView and print the text to the logs
-
+        val second = findViewById<TextView>(R.id.menu_item_2)
+        Log.v("2nd tag", second.text as String?)
         // Find third menu item TextView and print the text to the logs
+        val third= findViewById<TextView>(R.id.menu_item_3)
+        Log.v("Within printToLogs", third.text as String?)
 
+        //The tag text is only displayed once per function call, I wonder what will happen if that
+        //Tag text is different for each Log.v invocation
+        //Yep, every consecutive tag with the same text value will not be logged on invocation
     }
 }
