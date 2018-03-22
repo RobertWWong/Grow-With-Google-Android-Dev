2374d5ce0b40aafa68fcc7830c16c2157a31200e
Robert Wong <robertww@uci.edu>
2018-03-22 15:10:10 -0700

    Demonstrate Intent via practiceIntent, which open an email with a receipt

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.kt
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.kt
@@ -9,12 +9,16 @@
 package com.example.android.justjava
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+//import android.util.Log
 import android.view.View
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
+import java.net.URI
 
 /**
  * This app displays an order form to order coffee.
@@ -42,38 +46,57 @@ class MainActivity : AppCompatActivity() {
         priceTextView.text = text
     }
 
-    private fun calculatePrice(): Int {
-        return quantity * price
+    private fun calculatePrice(hasCream: Boolean, hasChocolate: Boolean): Int {
+        var extra = 0
+        if (hasCream)
+            extra += 1
+        if (hasChocolate)
+            extra += 2
+        return quantity * price + extra
     }
 
-    private fun calculatePrice(quantity: Int): Int {
-        return quantity * price
-    }
 
-    private fun calculatePrice(quantity: Int, cost: Int): Int {
-        return quantity * cost
-    }
 
     /**
      * This method is called when the order button is clicked}.
      */
     fun submitOrder(view: View) {
-        val total_price = calculatePrice()
         val checkBox = findViewById<CheckBox>(R.id.whipped_cream)
         val hasWhippedCream = checkBox.isChecked
         val chocoBox = findViewById<CheckBox>(R.id.chocolate_checkbox)
         val hasChocolate = chocoBox.isChecked
 
-
+        val total_price = calculatePrice(hasCream =hasWhippedCream, hasChocolate = hasChocolate)
 //        Log.v("Is checkbox clicked?", hasWhippedCream.toString())
 
         val str_msg = createOrderSummary(total_price, hasWhippedCream, hasChocolate)
         displayMessage(str_msg)
     }
 
-    fun createOrderSummary(total_price: Int, hasCream: Boolean, hasChocolate: Boolean): String {
+    fun practiceIntents(view: View) {
+        val checkBox = findViewById<CheckBox>(R.id.whipped_cream)
+        val hasWhippedCream = checkBox.isChecked
+        val chocoBox = findViewById<CheckBox>(R.id.chocolate_checkbox)
+        val hasChocolate = chocoBox.isChecked
+        val total_price = calculatePrice(hasCream =hasWhippedCream, hasChocolate = hasChocolate)
 
-        var msg = "Name Kaptain Kunai\n"
+        val name = findViewById<EditText>(R.id.name_field)
+        val str_msg = createOrderSummary(total_price, hasWhippedCream, hasChocolate)
+
+        val msg = "JustJava(kotlin, really) order for %s\n".format(if (!name.text.toString().isEmpty()) name.text.toString() else "Kaptain Kunai")
+        val intent = Intent(Intent.ACTION_SENDTO)
+
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_SUBJECT, msg)
+        intent.putExtra(Intent.EXTRA_TEXT, str_msg)
+
+        if (intent.resolveActivity(packageManager) != null)
+            startActivity(intent)
+    }
+
+    fun createOrderSummary(total_price: Int, hasCream: Boolean, hasChocolate: Boolean): String {
+        val name = findViewById<EditText>(R.id.name_field)
+        var msg = "Name: %s\n".format(if (!name.text.toString().isEmpty()) name.text.toString() else "Kaptain Kunai")
         msg += "Add whipped cream? %s\n".format(if (hasCream == true) "True" else "False")
         msg += "Add chocolate? %s\n".format(if (hasChocolate == true) "True" else "False")
         msg += "Quantity: %d\nTotal: $%d\nThank You!".format(quantity, total_price)
@@ -82,7 +105,8 @@ class MainActivity : AppCompatActivity() {
 
     //Need to figure out a way for functions to take input onClick, as writing two separate yet similiar functions is meh
     fun incr(view: View) {
-        quantity += 1;
+        if (quantity < 100)
+            quantity += 1;
         display(quantity)
     }
 
